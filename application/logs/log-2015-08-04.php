07b65f37a3ce1744f1c09adcd5307f747fd59e6c
Dong Dung <tiendungdong@gmail.com>
2015-08-04 18:02:15 +0000

    add demo.info

--- application/logs/log-2015-08-04.php
+++ application/logs/log-2015-08-04.php
@@ -1,3 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 ERROR - 2015-08-04 08:09:20 --> You did not select a file to upload.
+ERROR - 2015-08-04 16:57:58 --> The image you are attempting to upload exceedes the maximum height or width.
+ERROR - 2015-08-04 16:59:26 --> The image you are attempting to upload exceedes the maximum height or width.
+ERROR - 2015-08-04 17:02:30 --> You did not select a file to upload.
+ERROR - 2015-08-04 18:01:36 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
